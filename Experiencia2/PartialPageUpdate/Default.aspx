2f9832a84102a389e7b91bf123299f4bfdd2870f
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2021-11-28 12:20:59 -0500

    Se culmino con la implementacion de los ejercicios propuestos en la experiencia de practicas 2

---
+++ Experiencia2/PartialPageUpdate/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PartialPageUpdate._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        
+        <asp:ScriptManagerProxy ID="ScriptManagerProxy1" runat="server">
+        </asp:ScriptManagerProxy>
+        <br />
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:Label ID="Label1" runat="server" Text="Label" Width="196px"></asp:Label>
+        <br />
+                <br />
+                <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Page_Load" ValidateRequestMode="Enabled">
+                </asp:Timer>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Label" Width="196px"></asp:Label>
+        <br />
+        <br />
+        
+    </div>
+
+</asp:Content>
---
+++ Experiencia2/PartialPageUpdate/Default.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PartialPageUpdate
+{
+
+
+    public partial class _Default
+    {
+
+        /// <summary>
+        /// Control ScriptManagerProxy1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManagerProxy ScriptManagerProxy1;
+
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control Timer1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.Timer Timer1;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+    }
+}
